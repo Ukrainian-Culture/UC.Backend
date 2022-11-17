36a0c279a615d67f38508498897377c25fbfda9c
pflofif <bogdanvivchar8@gmail.com>
2022-11-17 19:15:15 +0200

    add entities project to hold all Db data

---
+++ Entities/Configurations/UsersConfigurations.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Entities.Configurations;
+
+public class UsersConfigurations : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasData(
+            new User
+            {
+                Id = 1,
+                Age = 14,
+                Name = "Vadym"
+            },
+            new User
+            {
+                Id = 2,
+                Age = 18,
+                Name = "Bohdan"
+            }
+        );
+    }
+}
---
+++ Entities/RepositoryContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Configurations;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities;
+
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UsersConfigurations());
+    }
+
+    public DbSet<User> Users { get; set; }
+}
