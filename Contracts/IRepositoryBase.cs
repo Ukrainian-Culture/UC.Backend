241ee7e6aa1cbfb3835241f6134c942798da48ff
pflofif <bogdanvivchar8@gmail.com>
2022-12-30 20:49:10 +0200

    In project Contracts  we are going to keep our interfaces. In project Repositories we are going to keep our repositories Create and implement RepositoryBase and UserRepository

---
+++ Contracts/IRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace Contracts;
+
+public interface IRepositoryBase<T>
+{
+    IQueryable<T> FindAll(ChangesType trackChanges);
+    IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, ChangesType trackChanges);
+    void Create(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositories;
+
+public class UserRepository : RepositoryBase<User> , IUserRepository
+{
+    public UserRepository(RepositoryContext context)
+        : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<User>> GetAllUsersAsync(ChangesType trackChanges)
+        => await FindAll(trackChanges)
+            .ToListAsync();
+
+    public async Task<User> GetUserByIdAsync(int id, ChangesType trackChanges)
+        => await FindByCondition(user => user.Id == id , trackChanges)
+            .SingleAsync();
+
+    public void CreateUser(User user) => Create(user);
+    public void UpdateUser(User user) => Update(user);
+    public void DeleteUser(User user) => Delete(user);
+}
