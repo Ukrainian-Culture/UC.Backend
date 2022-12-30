b5533fdf0c7c8d2338620605b8fa5441add72d24
pflofif <bogdanvivchar8@gmail.com>
2022-12-30 21:14:21 +0200

    Add repository interfaces . All interfaces are empty because we will supplement them during the other tasks

---
+++ Contracts/IRepositoryManager.cs
@@ -0,0 +1,12 @@
+﻿namespace Contracts;
+
+public interface IRepositoryManager
+{
+    IUserRepository Users { get; }
+    IArticleLocalesRepository ArticleLocales { get; }
+    IArticleRepository Articles { get; }
+    ICategoryLocalesRepository CategoryLocales { get; }
+    ICategoryRepository Categories { get; }
+    IInfoRepository Infos { get; }
+    Task SaveAsync();
+}
---
+++ Repositories/InfoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+
+namespace Repositories;
+
+public class InfoRepository : RepositoryBase<Info>, IInfoRepository
+{
+    public InfoRepository(RepositoryContext context)
+        : base(context)
+    {
+    }
+}
