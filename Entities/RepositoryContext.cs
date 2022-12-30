5a35358fdb6710df3905f831f77566591a93ff00
pflofif <bogdanvivchar8@gmail.com>
2022-12-30 16:57:42 +0200

    1. Add RepositoryContext class for interaction with DB 2. Relationships between tables are configured 3. CategoryLocale and ArticlesLocale have composit keys

---
+++ Entities/RepositoryContext.cs
@@ -0,0 +1,33 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities;
+
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CategoryLocale>().HasKey(ct => new { ct.CategoryId, ct.CultureId });
+        modelBuilder.Entity<ArticlesLocale>().HasKey(article => new { article.Id, article.CultureId });
+
+        modelBuilder.Entity<Culture>()
+            .HasMany(cul => cul.ArticlesTranslates)
+            .WithOne(a => a.Culture);
+
+        modelBuilder.Entity<Culture>()
+            .HasMany(cul => cul.Categories)
+            .WithOne(a => a.Culture);
+    }
+
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<Article> Articles { get; set; } = null!;
+    public DbSet<Info> Info { get; set; } = null!;
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<CategoryLocale> CategoryLocales { get; set; } = null!;
+    public DbSet<ArticlesLocale> ArticlesLoxaLocales { get; set; } = null!;
+    public DbSet<Culture> Cultures { get; set; } = null!;
+}
