da552e3ca3e73232c17ee6e02f3a8d2adbf086c5
pflofif <bogdanvivchar8@gmail.com>
2023-01-02 17:04:07 +0200

    Create Logging class

--- Ukranian-Culture.Backend/Program.cs
+++ Ukranian-Culture.Backend/Program.cs
@@ -1,9 +1,14 @@
 using Contracts;
 using Entities;
 using Microsoft.EntityFrameworkCore;
+using NLog;
 using Repositories;
+using Ukranian_Culture.Backend;
 
 var builder = WebApplication.CreateBuilder(args);
+
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), @"\nlog.config"));
+builder.Services.AddScoped<ILoggerManager, LoggerManager>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -12,13 +17,13 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<RepositoryContext>(
     opts => opts.UseSqlServer(
-        builder.Configuration.GetConnectionString("Db_connection"),
+        builder.Configuration.GetConnectionString("Db_connection")!,
         b => b.MigrationsAssembly("Ukranian-Culture.Backend")
     )
 );
 builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
 var app = builder.Build();
-var logger = NLog.LogManager.GetCurrentClassLogger();
+var logger = LogManager.GetCurrentClassLogger();
 
 var services = (IServiceScopeFactory)app.Services.GetService(typeof(IServiceScopeFactory))!;
 using (var db = services.CreateScope().ServiceProvider.GetService<RepositoryContext>())
