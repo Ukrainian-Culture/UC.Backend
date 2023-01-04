3442a3f375c54595b6aff200b79422e64cd0bc5e
Petro Horokh <petrohorokh@gmail.com>
2023-01-04 19:52:11 +0200

    Add GetAllArticlesLocale

--- Contracts/IArticleLocalesRepository.cs
+++ Contracts/IArticleLocalesRepository.cs
@@ -5,6 +5,7 @@ namespace Contracts;
 public interface IArticleLocalesRepository
 {
     public Task<ArticlesLocale> GetArticlesLocaleByIdAsync(int id, ChangesType trackChanges);
+    public IEnumerable<ArticlesLocale> GetAllArticlesLocale(ChangesType trackChanges);
 
     void CreateArticlesLocale(ArticlesLocale article);
     void UpdateArticlesLocale(ArticlesLocale article);
--- Repositories/ArticleLocalesRepository.cs
+++ Repositories/ArticleLocalesRepository.cs
@@ -15,6 +15,10 @@ public class ArticleLocalesRepository : RepositoryBase<ArticlesLocale>, IArticle
     public async Task<ArticlesLocale> GetArticlesLocaleByIdAsync(int id, ChangesType trackChanges)
        => await FindByCondition(art => art.Id == id, trackChanges)
            .SingleAsync();
+
+    public IEnumerable<ArticlesLocale> GetAllArticlesLocale(ChangesType trackChanges)
+        => FindAll(trackChanges).ToList();
+
     public void CreateArticlesLocale(ArticlesLocale articleL) => Create(articleL);
     public void UpdateArticlesLocale(ArticlesLocale articleL) => Update(articleL);
     public void DeleteArticlesLocale(ArticlesLocale articleL) => Delete(articleL);
