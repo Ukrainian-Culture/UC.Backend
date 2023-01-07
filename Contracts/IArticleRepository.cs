6f2f8a2bc7cb9b4eada58e81a43726635a59a781
pflofif <bogdanvivchar8@gmail.com>
2023-01-07 21:46:04 +0200

    Add 2 DTOs model and Add new methods to ArticleRepository

--- Contracts/IArticleRepository.cs
+++ Contracts/IArticleRepository.cs
@@ -9,6 +9,8 @@ public interface IArticleRepository
     Task<IEnumerable<Article>> GetAllByConditionAsync(Expression<Func<Article, bool>> expression,
         ChangesType trackChanges);
 
+    Task<Article?> GetFirstByConditionAsync(Expression<Func<Article, bool>> expression, ChangesType trackChanges);
+
     void CreateArticle(Article article);
     void UpdateArticle(Article article);
     void DeleteArticle(Article article);
