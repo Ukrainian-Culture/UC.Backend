b982338cc5256980f5d02ba2ccf96b924bb0f1c9
Petro Horokh <petrohorokh@gmail.com>
2023-01-19 18:39:59 +0200

    Add Parsers interface and class

---
+++ Parsers/Parser.cs
@@ -0,0 +1,18 @@
+﻿using HtmlAgilityPack;
+
+namespace Parsers;
+public class Parser : IParser
+{
+    public async Task<HtmlNodeCollection> GetNodeByUrl(string _url, string _node)
+    {
+        var html = _url;
+
+        HtmlWeb web = new HtmlWeb();
+
+        var htmlDoc = web.Load(html);
+
+        var node = htmlDoc.DocumentNode.SelectNodes(_node);
+
+        return node;
+    }
+}
