ce9c3f0fb1f019df974fb9ac0eb9079be26e0f00
34r7389fe709f <111686428+34r7389fe709f@users.noreply.github.com>
2023-01-23 04:15:13 +0200

    Update unit tests

--- Ukrainian-Culture.Tests/ControllersTests/AccountControllerTests.cs
+++ Ukrainian-Culture.Tests/ControllersTests/AccountControllerTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Routing;
 
 namespace Ukrainian_Culture.Tests.ControllersTests;
 
@@ -10,7 +12,7 @@ public class AccountControllerTests
     public async Task SignUp_ShouldReturnOkStatus_WhenUserDataIsCorrect()
     {
         //arrange
-        _account.SignUpAsync(Arg.Any<SignUpUser>()).Returns(IdentityResult.Success);
+        _account.SignUpAsync(Arg.Any<SignUpUser>(), Arg.Any<HttpContext>(), Arg.Any<UrlHelper>()).Returns(IdentityResult.Success);
         var controller = new AccountController(_account);
         var user = new SignUpUser()
         {
@@ -32,7 +34,7 @@ public class AccountControllerTests
     public async Task SignUp_ShouldReturnNull_WhenInvalidPassword()
     {
         //arrange
-        _account.SignUpAsync(Arg.Any<SignUpUser>()).Returns(IdentityResult.Failed());
+        _account.SignUpAsync(Arg.Any<SignUpUser>(), Arg.Any<HttpContext>(), Arg.Any<UrlHelper>()).Returns(IdentityResult.Failed());
         var controller = new AccountController(_account);
         var user = new SignUpUser()
         {
@@ -89,5 +91,35 @@ public class AccountControllerTests
 
         //assert
         result.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task ConfirmEmailAsync_ShouldReturnFalse_WhenUserDoesNotExistsOrCodeIsInvalid()
+    {
+        //arrange
+        bool expected = false;
+        _account.ConfirmEmailAsync(Arg.Any<string>(), Arg.Any<string>()).Returns(expected);
+        var controller = new AccountController(_account);
+        var userId = "123";
+        var code = "CfDJ8KMcWVA73Z9EpmagZUfXsP6OjqaQf8JHGuWfQ%2Brv%2FBLqADK4CvEIRJk0lx1i5t8rLXddkxN%2BQEUqooEAHqAo4a50TLPKiAbLSev4WzlEJywh39RoaDH04EfuIPfvL2IG2kQZEtSNYv5M4%2FSEbnDbzyya0s8ScLHMrg%2BOnG31wXpqGTC1rfHmsFn8gfflgknmVqcuUUiYNi3velL7vLMYf91%2B%2F7wFhQZvwljdkXpQ5g%2Fxx%2FOGZBonNraB5mFOXIy3Qw%3D%3D";
+        //act
+        var result = await controller.ConfirmEmail(userId, code) as OkObjectResult;
+        
+        //assert
+        result.Equals(expected);
+    }
+    public async Task ConfirmEmailAsync_ShouldReturnTrue_WhenEmailWasConfirmed()
+    {
+        //arrange
+        bool expected = true;
+        _account.ConfirmEmailAsync(Arg.Any<string>(), Arg.Any<string>()).Returns(expected);
+        var controller = new AccountController(_account);
+        var userId = "5ba6d703-fa9e-4771-561e-08dafcd8b602";
+        var code = "CfDJ8KMcWVA73Z9EpmagZUfXsP6OjqaQf8JHGuWfQ%2Brv%2FBLqADK4CvEIRJk0lx1i5t8rLXddkxN%2BQEUqooEAHqAo4a50TLPKiAbLSev4WzlEJywh39RoaDH04EfuIPfvL2IG2kQZEtSNYv5M4%2FSEbnDbzyya0s8ScLHMrg%2BOnG31wXpqGTC1rfHmsFn8gfflgknmVqcuUUiYNi3velL7vLMYf91%2B%2F7wFhQZvwljdkXpQ5g%2Fxx%2FOGZBonNraB5mFOXIy3Qw%3D%3D";
+        //act
+        var result = await controller.ConfirmEmail(userId, code) as OkObjectResult;
+
+        //assert
+        result.Equals(expected);
     }
 }
