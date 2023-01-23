250a579b0f44604463bbca202be2760e8eeb750f
34r7389fe709f <111686428+34r7389fe709f@users.noreply.github.com>
2023-01-23 14:16:33 +0200

    Add function SendEmail and ConfrimEmailAsync to AccountRepository

--- Contracts/IAccountRepository.cs
+++ Contracts/IAccountRepository.cs
@@ -1,11 +1,14 @@
 ﻿using Entities.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Contracts;
 
 public interface IAccountRepository
 {
-    Task<IdentityResult> SignUpAsync(SignUpUser signUpModel);
+    Task<IdentityResult> SignUpAsync(SignUpUser signUpModel, HttpContext httpContext, IUrlHelper url);
     Task<string> LoginAsync(SignInUser signInModel);
+    Task<bool> ConfirmEmailAsync(string userId, string code);
 }
 
