c366583e0d87496088630a57a45d5430f899824d
pflofif <bogdanvivchar8@gmail.com>
2023-01-24 18:44:42 +0200

    Add new method to ArticlesTileController.cs to get all articles as Dictionary. Ad unit tests to this method

--- Ukrainian-Culture.Tests/ControllersTests/ArticleTileControllerTests.cs
+++ Ukrainian-Culture.Tests/ControllersTests/ArticleTileControllerTests.cs
@@ -7,6 +7,7 @@ public class ArticleTileControllerTests
     private readonly IRepositoryManager _repositoryManager = Substitute.For<IRepositoryManager>();
     private readonly IMapper _mapper = Substitute.For<IMapper>();
     private readonly ILoggerManager _logger = Substitute.For<ILoggerManager>();
+
     [Fact]
     public async Task GetAllArticlesOnLanguage_ShouldReturnListOfArticlesTileDto_WhenDbIsNotEmpty()
     {
@@ -16,15 +17,15 @@ public class ArticleTileControllerTests
         Guid cultureId = new("5eca5808-4f44-4c4c-b481-72d2bdf24111");
 
         _repositoryManager.Articles
-                .GetAllByConditionAsync(Arg.Any<Expression<Func<Article, bool>>>(), Arg.Any<ChangesType>())
-                .Returns(new List<Article>()
-                {
+            .GetAllByConditionAsync(Arg.Any<Expression<Func<Article, bool>>>(), Arg.Any<ChangesType>())
+            .Returns(new List<Article>()
+            {
                 new()
                 {
                     Id = articleId,
                     CategoryId = categoryId,
                 }
-                });
+            });
 
         _repositoryManager.Cultures.GetCultureWithContentAsync(cultureId, ChangesType.AsNoTracking)
             .Returns(new Culture()
@@ -167,5 +168,64 @@ public class ArticleTileControllerTests
         statusCode.Should().Be((int)HttpStatusCode.OK); //HttpStatusCode.OK = 200
         resultArray.Should().HaveCount(1);
         _mapper.ReceivedCalls().Should().HaveCount(1);
+    }
+
+    [Fact]
+    public async Task GetArticlesTileForEveryCategory_ShouldReturnOkWithCorrectValue_WhenAllDaraIsCorect()
+    {
+        //Arrange
+        Guid articleId = new("5eca5808-4f44-4c4c-b481-72d2bdf24203");
+        Guid categoryId = new("5b32effd-2636-4cab-8ac9-3258c746aa53");
+        Guid cultureId = new("5eca5808-4f44-4c4c-b481-72d2bdf24111");
+
+        _repositoryManager.Articles
+            .GetAllByConditionAsync(Arg.Any<Expression<Func<Article, bool>>>(), Arg.Any<ChangesType>())
+            .Returns(new List<Article>
+            {
+                new()
+                {
+                    Id = articleId,
+                    CategoryId = categoryId,
+                }
+            });
+
+        _repositoryManager.Cultures.GetCultureWithContentAsync(cultureId, ChangesType.AsNoTracking)
+            .Returns(new Culture
+            {
+                Id = cultureId,
+                ArticlesTranslates = new List<ArticlesLocale>
+                {
+                    new()
+                    {
+                        Id = articleId,
+                        CultureId = cultureId,
+                    }
+                },
+                Categories = new List<CategoryLocale>
+                {
+                    new()
+                    {
+                        CategoryId = categoryId,
+                        CultureId = cultureId,
+                    }
+                }
+            });
+        _mapper.Map<ArticleTileDto>(Arg.Any<(Article, ArticlesLocale)>())
+            .Returns(new ArticleTileDto
+            {
+                ArticleId = articleId,
+                Category = "first"
+            });
+        var controller = new ArticlesTileController(_repositoryManager, _mapper, _logger);
+        
+        //Act
+        var result = await controller.GetArticlesTileForEveryCategory(cultureId) as OkObjectResult;
+        var statusCode = result!.StatusCode;
+        var value = (Dictionary<string, List<ArticleTileDto>>)result.Value!;
+        
+        //Assert
+        value.Should().HaveCount(1);
+        value["first"].First().ArticleId.Should().Be(articleId);
+        statusCode.Should().Be((int)HttpStatusCode.OK);
     }
 }
