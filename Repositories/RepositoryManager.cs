cd5dd041b8d91ff067369714dca0e7cae4a14877
pflofif <bogdanvivchar8@gmail.com>
2023-02-25 13:24:10 +0200

    Refactor code : Using Lazy<> in DI , because its thread safety

--- Repositories/RepositoryManager.cs
+++ Repositories/RepositoryManager.cs
@@ -6,34 +6,42 @@ namespace Repositories;
 public class RepositoryManager : IRepositoryManager
 {
     private readonly RepositoryContext _repositoryContext;
-    private IUserRepository? _userRepository;
-    private IArticleLocalesRepository? _articleLocalesRepository;
-    private IArticleRepository? _articleRepository;
-    private ICategoryRepository? _categoryRepository;
-    private ICultureRepository? _cultureRepository;
-    private IUserHistoryRepository? _userHistoryRepository;
-    public RepositoryManager(RepositoryContext repositoryContext)
+    private readonly Lazy<IUserRepository> _userRepository;
+    private readonly Lazy<IArticleLocalesRepository> _articleLocalesRepository;
+    private readonly Lazy<IArticleRepository> _articleRepository;
+    private readonly Lazy<ICategoryRepository> _categoryRepository;
+    private readonly Lazy<ICultureRepository> _cultureRepository;
+    private readonly Lazy<IUserHistoryRepository> _userHistoryRepository;
+
+    public RepositoryManager(
+        RepositoryContext repositoryContext,
+        Lazy<IUserRepository> userRepository,
+        Lazy<IArticleLocalesRepository> articleLocalesRepository,
+        Lazy<IArticleRepository> articleRepository,
+        Lazy<ICategoryRepository> categoryRepository,
+        Lazy<ICultureRepository> cultureRepository,
+        Lazy<IUserHistoryRepository> userHistoryRepository)
     {
         _repositoryContext = repositoryContext;
+        _userRepository = userRepository;
+        _articleLocalesRepository = articleLocalesRepository;
+        _articleRepository = articleRepository;
+        _categoryRepository = categoryRepository;
+        _cultureRepository = cultureRepository;
+        _userHistoryRepository = userHistoryRepository;
     }
 
-    public IUserRepository Users
-        => _userRepository ??= new UserRepository(_repositoryContext);
+    public IUserRepository Users => _userRepository.Value;
 
-    public IArticleLocalesRepository ArticleLocales
-        => _articleLocalesRepository ??= new ArticleLocalesRepository(_repositoryContext);
+    public IArticleLocalesRepository ArticleLocales => _articleLocalesRepository.Value;
 
-    public IArticleRepository Articles
-        => _articleRepository ??= new ArticleRepository(_repositoryContext);
+    public IArticleRepository Articles => _articleRepository.Value;
 
-    public ICategoryRepository Categories
-        => _categoryRepository ??= new CategoryRepository(_repositoryContext);
+    public ICategoryRepository Categories => _categoryRepository.Value;
 
-    public ICultureRepository Cultures
-        => _cultureRepository ??= new CultureRepository(_repositoryContext);
+    public ICultureRepository Cultures => _cultureRepository.Value;
 
-    public IUserHistoryRepository UserHistory
-        => _userHistoryRepository ??= new UserHistoryRepository(_repositoryContext);
+    public IUserHistoryRepository UserHistory => _userHistoryRepository.Value;
 
     public async Task SaveAsync() => await _repositoryContext.SaveChangesAsync();
 }
